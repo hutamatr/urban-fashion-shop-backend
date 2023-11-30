6adfa301f0b4ffde01c95f7d3af374250633ea0f
hutamatr <hutamatr@gmail.com>
2023-11-30 19:33:54 +0700

    feat: create user sequelize model

---
+++ src/v1/models/userModel.ts
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize';
+
+import { sequelize } from '../../database/db';
+
+/**
+ * The code is defining a Sequelize model called "User" with the specified attributes and options. **/
+const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    last_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+);
+
+export default User;
