7fb7ca5bb1ad54b97594769787d8eaeb901750b8
hutamatr <hutamatr@gmail.com>
2023-12-06 00:24:39 +0700

    feat: create cart item sequelize model

---
+++ src/v1/models/cartItemModel.ts
@@ -0,0 +1,55 @@
+import { DataTypes, ModelDefined, Optional } from 'sequelize';
+
+import { sequelize } from '../../database/db';
+
+interface ICartItem {
+  id: number;
+  quantity: number;
+  cart_id: number;
+  product_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+type CartItemCreationAttributes = Optional<
+  ICartItem,
+  'id' | 'created_at' | 'updated_at'
+>;
+
+const CartItem: ModelDefined<ICartItem, CartItemCreationAttributes> =
+  sequelize.define(
+    'cart_item',
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cart_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        references: {
+          model: 'carts',
+          key: 'id',
+        },
+      },
+      product_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+      },
+    },
+    {
+      underscored: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
+
+export default CartItem;
