80a1356e23ccb54bd2f8f718527586a43a7c144b
hutamatr <hutamatr@gmail.com>
2023-12-05 22:25:25 +0700

    feat: create user role model

---
+++ src/v1/models/roleModel.ts
@@ -0,0 +1,39 @@
+import { DataTypes, ModelDefined, Optional } from 'sequelize';
+
+import { sequelize } from '../../database/db';
+
+interface IRole {
+  id: number;
+  role_name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+type RoleCreationAttributes = Optional<
+  IRole,
+  'id' | 'created_at' | 'updated_at'
+>;
+
+/* The code is defining a Sequelize model for a category entity. */
+const Role: ModelDefined<IRole, RoleCreationAttributes> = sequelize.define(
+  'role',
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    role_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+);
+
+export default Role;
