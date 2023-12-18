a5498d33e236ae663b9c843e095860fbcb1e7b39
hutamatr <hutamatr@gmail.com>
2023-12-18 23:10:39 +0700

    feat: create wishlist routes

---
+++ src/v1/routes/wishlistRoutes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import {
+  createWishlist,
+  deleteWishlist,
+  getWishlist,
+  getWishlists,
+} from '../controllers/wishlistController';
+import { authMiddleware } from '../../middleware/auth';
+
+const router = Router();
+
+router.get('/', authMiddleware, getWishlists);
+
+router.get('/:productId', authMiddleware, getWishlist);
+
+router.post('/', authMiddleware, createWishlist);
+
+router.delete('/:productId', authMiddleware, deleteWishlist);
+
+export default router;
