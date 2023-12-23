e0b96f4b35cf62db7f10c0e57ba6d67ac4ba464b
hutamatr <hutamatr@gmail.com>
2023-12-23 16:17:42 +0700

    feat: change filename & create delete img from storage func

---
+++ src/database/firebaseStorage.ts
@@ -0,0 +1,43 @@
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from 'firebase/storage';
+global.XMLHttpRequest = require('xhr2');
+
+import { storage } from './firebaseConfig';
+
+export async function imageUpload(file: Express.Multer.File, url: string) {
+  const storageRef = ref(storage, url);
+
+  const timeStamp = Date.now();
+  const name = file?.originalname.split('.')[0];
+  const type = file?.originalname.split('.')[1];
+  const fileName = `${name}-${timeStamp}.${type}`;
+
+  const imageRef = ref(storageRef, fileName);
+  const snapShot = await uploadBytes(imageRef, file?.buffer);
+  const downloadURL = await getDownloadURL(snapShot.ref);
+
+  return { downloadURL };
+}
+
+export async function deleteImageFromStorage(imageURL: string, url: string) {
+  // Use a regular expression to match the encoded filename
+  // eslint-disable-next-line no-useless-escape
+  const match = RegExp(/\/([^\/?#]+)(?:[?#]|$)/).exec(imageURL);
+
+  // Extract the matched result and then decode the URL-encoded filename
+  const encodedFilename = match ? match[1] : null;
+  const decodedFilename = encodedFilename
+    ? decodeURIComponent(encodedFilename)
+    : null;
+
+  // Remove "products/products/" from the filename
+  const finalFilename = decodedFilename?.replace(/^products\//, '');
+
+  const storageRef = ref(storage, url);
+  const imageRef = ref(storageRef, finalFilename);
+  await deleteObject(imageRef);
+}
