b051f461300279f982c8ab04023f33bc4a80fd21
hutamatr <hutamatr@gmail.com>
2024-01-31 20:32:52 +0700

    refactor: refactor db init config for dev, test, prod

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,18 +1,56 @@
-import { Sequelize } from 'sequelize';
+import { Dialect, Sequelize } from 'sequelize';
 
-import { dbName, dbPassword, dbUser } from '../utils/constants';
+import {
+  dbHostDevelopment,
+  dbHostProduction,
+  dbHostTest,
+  dbNameDevelopment,
+  dbNameProduction,
+  dbNameTest,
+  dbPassword,
+  dbUser,
+  env,
+} from '../utils/constants';
 
-/** The code is creating a new instance of the Sequelize class and assigning it to the constant
- * `sequelize`. The Sequelize class is a constructor function provided by the Sequelize
- * library, which is an Object-Relational Mapping (ORM) tool for Node.js.
- **/
-export const sequelize = new Sequelize({
-  database: dbName,
-  username: dbUser,
-  password: dbPassword,
-  host: 'localhost',
-  dialect: 'mysql',
-  timezone: '+07:00',
-  // eslint-disable-next-line no-console
-  logging: console.log,
-});
+const dbConfig: {
+  [key: string]: {
+    username: string | undefined;
+    password: string | undefined;
+    database: string | undefined;
+    host: string | undefined;
+    dialect: Dialect | undefined;
+    timezone: string | undefined;
+    logging?: boolean;
+  };
+} = {
+  development: {
+    username: dbUser,
+    password: dbPassword,
+    database: dbNameDevelopment,
+    host: dbHostDevelopment,
+    dialect: 'mysql',
+    timezone: '+07:00',
+    logging: true,
+  },
+  test: {
+    username: dbUser,
+    password: dbPassword,
+    database: dbNameTest,
+    host: dbHostTest,
+    dialect: 'mysql',
+    timezone: '+07:00',
+    logging: false,
+  },
+  production: {
+    username: dbUser,
+    password: dbPassword,
+    database: dbNameProduction,
+    host: dbHostProduction,
+    dialect: 'mysql',
+    timezone: '+07:00',
+    logging: false,
+  },
+};
+
+const databaseConfig = dbConfig[env || 'development'];
+export const sequelize = new Sequelize(databaseConfig);
